62685ed9f9436492ef344a169f97b15e68ccf846
JOSELO <JOSELO@DESKTOP-26KMVBP>
2020-09-26 12:48:37 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport11 = new WindowsFormsApp3.CrystalReport1();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mostrar todos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.crystalReportViewer1.InitialFocus = CrystalDecisions.Windows.Forms.UIComponent.ToolBar;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(299, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.CrystalReport11;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(916, 658);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(222, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ocultar campos";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(24, 62);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Telefono";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(119, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Dirección";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(234, 101);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 269);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(251, 101);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 58);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 28);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mostrar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 658);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private CrystalReport1 CrystalReport11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,132 @@
+﻿using CrystalDecisions.ReportAppServer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        JosephTutos_PedidosEntities JosephTutos;
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Items.Add("Todos");
+            comboBox2.Items.Add(2);
+            comboBox2.Items.Add(4);
+            comboBox2.Items.Add(6);
+            comboBox2.Items.Add(8);
+            comboBox2.Items.Add(10);
+
+            JosephTutos = new JosephTutos_PedidosEntities();
+            var sp = JosephTutos.SP_Clientes("");
+
+            clientes = new List<cliente>();
+            clientes = await JosephTutos.cliente.ToListAsync();
+
+            comboBox1.DataSource = clientes;
+            comboBox1.DisplayMember = "clie_nombre";
+            comboBox1.ValueMember = "clie_id";
+            //comboBox1.Items.Add("Todos");
+        }
+
+        List<cliente> clientes;
+        private async void button1_Click(object sender, EventArgs e)
+        {
+                await CargarClientes(null, 0, checkBox1.Checked, checkBox2.Checked);
+        }
+
+
+        async Task CargarClientes(int? id, int cantidadRegistros, bool tel, bool direccion)
+        {
+            JosephTutos = new JosephTutos_PedidosEntities();
+            var clientesBD = await JosephTutos.cliente.ToListAsync();
+            if (string.IsNullOrEmpty(id.ToString()))
+            {
+                clientes = clientesBD;
+            }
+            else
+            {
+                clientes = clientesBD.Where(s => s.clie_id == id).ToList();
+            }
+
+            if (cantidadRegistros > 0)
+            {
+                clientes = clientesBD.Take(cantidadRegistros).ToList();
+            }
+
+            if (tel)
+            {
+                clientes.ForEach(s => s.clie_telefono = "");
+            }
+
+            if (direccion)
+            {
+                clientes.ForEach(s => s.clie_direccion = "");
+            }
+
+            var cry = new CrystalReport1();
+
+            this.crystalReportViewer1.ReportSource = cry;
+
+            cry.SetDataSource(clientes);
+
+            this.crystalReportViewer1.RefreshReport();
+            
+            clientesBD = null;
+            clientes = null;
+        }
+
+
+        private async void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var valor = comboBox2.Text;
+            if (valor == "Todos")
+            {
+                valor = "0";
+            }
+            await CargarClientes(null, int.Parse(valor), checkBox1.Checked, checkBox2.Checked);
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "Todos")
+                {
+                    await CargarClientes(null, 0, checkBox1.Checked, checkBox2.Checked);
+                    return;
+                }
+
+                var valor = int.Parse(comboBox1.SelectedValue.ToString());
+                await CargarClientes(valor, 0, checkBox1.Checked, checkBox2.Checked);
+            }
+            catch (Exception)
+            {
+
+            }
+           
+        }
+
+        private async void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            await CargarClientes(0, 0, checkBox1.Checked, checkBox2.Checked);
+        }
+
+        private async void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            await CargarClientes(0, 0, checkBox1.Checked, checkBox2.Checked);
+        }
+    }
+}
